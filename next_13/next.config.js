84557aadb69f0588f70734d27bc555b66a2676f9
rayprastya <rayhan.prastya@gmail.com>
2023-04-11 04:56:08 +0700

    add next.config

--- next_13/next.config.js
+++ next_13/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/landingdev',
+  trailingSlash: true,
   experimental: {
     appDir: true,
   },
   reactStrictMode: true,
+  output: "export" 
 }
 
 module.exports = nextConfig
