2e1faf34f540e3ea9dcb529eb5e7466cd86f9b38
gc sena <garuda.casasena@ifabula.com>
2025-03-17 15:43:05 +0700

    reseller page responsive

--- react_18/src/pages/Reseller.jsx
+++ react_18/src/pages/Reseller.jsx
@@ -48,21 +48,21 @@ const Reseller = () => {
             <img className="amanda" src={amanda} alt="Avatar"></img>
           </div>
           <div className="col-xl col-lg col-md-1 col-sm">
-            <h1 className="headA hidden">
+            <h1 className="headF">
               BERGABUNG BERSAMA KAMI <br /> MENJADI RESELLER DREWHITE
             </h1>
-            <p className="txtA hidden">
+            <p className="txtF">
               Hasilkan uang ratusan juta dan dapatkan kesempatan untuk liburan
               ke Singapore bersama DR EKLES dan Amanda Manopo
             </p>
-            <h1 className="headA2 hidden">#BISNISHALALCUANLANCAR</h1>
+            <h1 className="headF">#BISNISHALALCUANLANCAR</h1>
             <a
               href="https://api.whatsapp.com/send/?phone=081211118412&text&type=phone_number&app_absent=0"
               target="_blank"
               rel="noopener noreferrer"
               className="buttonContainer"
             >
-              <button className="btnWA1 hidden">
+              <button className="btnWA">
                 <BsWhatsapp className="iconWa" />
                 GABUNG SEKARANG
               </button>
@@ -101,57 +101,57 @@ const Reseller = () => {
         </div>
       </div>
       <div className="container-fluid resellerC">
-      <h1 className="headC">
-        MANFAAT BERGABUNG MENJADI <br /> RESELLER DREWHITE
-      </h1>
+        <h1 className="headC hidden">
+          MANFAAT BERGABUNG MENJADI <br /> RESELLER DREWHITE
+        </h1>
 
-      <div className="row justify-content-center">
-        <div className="col-md-6 benefit-box">
-          <img className="resellerIcon" src={reseller5} alt="Benefit 1" />
-          <p className="manfaatP">
-            <b>Margin Profit Yang Menarik</b> <br />
-            Produk yang sudah teruji memiliki permintaan yang tinggi di pasaran serta margin profit yang sangat menguntungkan
-          </p>
+        <div className="row justify-content-center">
+          <div className="col-md-6 benefit-box hidden">
+            <img className="resellerIcon" src={reseller5} alt="Benefit 1" />
+            <p className="manfaatP">
+              <b>Margin Profit Yang Menarik</b> <br />
+              Produk yang sudah teruji memiliki permintaan yang tinggi di pasaran serta margin profit yang sangat menguntungkan
+            </p>
+          </div>
+
+          <div className="col-md-6 benefit-box hidden">
+            <img className="resellerIcon" src={reseller4} alt="Benefit 2" />
+            <p className="manfaatP">
+              <b>Benefit Potongan Harga dan Poin</b> <br />
+              Dapatkan harga khusus reseller dan poin untuk mendapatkan reward.
+            </p>
+          </div>
         </div>
 
-        <div className="col-md-6 benefit-box">
-          <img className="resellerIcon" src={reseller4} alt="Benefit 2" />
-          <p className="manfaatP">
-            <b>Benefit Potongan Harga dan Poin</b> <br />
-            Dapatkan harga khusus reseller dan poin untuk mendapatkan reward.
-          </p>
+        <div className="row justify-content-center">
+          <div className="col-md-6 benefit-box hidden">
+            <img className="resellerIcon" src={reseller3} alt="Benefit 3" />
+            <p className="manfaatP">
+              <b>Mendapatkan Reward Secara Berkala</b> <br />
+              Dapatkan reward menarik seperti jalan-jalan ke Luar Negeri bareng Amanda dan Dokter Ekles dan reward menarik lainnya
+            </p>
+          </div>
+
+          <div className="col-md-6 benefit-box hidden">
+            <img className="resellerIcon" src={reseller1} alt="Benefit 4" />
+            <p className="manfaatP">
+              <b>Dibantu Dalam Pengembangan Bisnis DREWHITE</b> <br />
+              Mendapatkan bimbingan pengembangan bisnis yang diadakan setiap seminggu sekali.
+            </p>
+          </div>
+        </div>
+
+
+        <div className="row justify-content-center">
+          <div className="col-md-6 benefit-box hidden">
+            <img className="resellerIcon" src={reseller2} alt="Benefit 5" />
+            <p className="manfaatP">
+              <b>Pelayanan Konsultasi Langsung Dengan Dokter</b> <br />
+              Pelayanan ekslusif konsultasi produk dan bisnis dengan Dr. Ekles / tim DREWHITE setiap bulan.
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="row justify-content-center">
-        <div className="col-md-6 benefit-box">
-          <img className="resellerIcon" src={reseller3} alt="Benefit 3" />
-          <p className="manfaatP">
-            <b>Mendapatkan Reward Secara Berkala</b> <br />
-            Dapatkan reward menarik seperti jalan-jalan ke Luar Negeri bareng Amanda dan Dokter Ekles dan reward menarik lainnya
-          </p>
-        </div>
-
-        <div className="col-md-6 benefit-box">
-          <img className="resellerIcon" src={reseller1} alt="Benefit 4" />
-          <p className="manfaatP">
-            <b>Dibantu Dalam Pengembangan Bisnis DREWHITE</b> <br />
-            Mendapatkan bimbingan pengembangan bisnis yang diadakan setiap seminggu sekali.
-          </p>
-        </div>
-      </div>
-
-     
-      <div className="row justify-content-center">
-        <div className="col-md-6 benefit-box">
-          <img className="resellerIcon" src={reseller2} alt="Benefit 5" />
-          <p className="manfaatP">
-            <b>Pelayanan Konsultasi Langsung Dengan Dokter</b> <br />
-            Pelayanan ekslusif konsultasi produk dan bisnis dengan Dr. Ekles / tim DREWHITE setiap bulan.
-          </p>
-        </div>
-      </div>
-    </div>
 
 
 
@@ -192,74 +192,36 @@ const Reseller = () => {
         </div>
       </div>
 
-
-
-      {/* <div className='resellerF'>
-          <h1 className='headC'>YUK LIBURAN KE SINGAPORE BARENG DR. EKLES & AMANDA MANOPO</h1>
-          <div className='row'>
-            <div className="col-xl col-lg col-md-1">
-
-        
-        </div>
-          </div>
-            </div> */}
-
-      <div className="resellerF" style={{ backgroundColor: "#FFE2EB" }}>
-        <h1 className="headF">BERGABUNGLAH DENGAN KAMI SEKARANG</h1>
-        <div className="joinWrapper row">
-          <div className="col-xl col-lg col-md-1">
-            <img className="resellerCoa" src={coa5} alt="Avatar"></img>
-          </div>
-          <div className="col-xl col-lg col-md-1">
-            <p className="txtF">
-              Ingin mencari kesempatan bisnis dengan modal kecil namun
-              berpotensi menghasilkan keuntungan besar? Jadilah reseller produk
-              kolagen drewhite. Bergabunglah dengan kami sekarang dan dapatkan
-              peluang untuk menawarkan produk berkualitas tinggi kepada
-              pelanggan Anda. Dengan produk kami yang terbukti efektif dan aman,
-              Anda dapat memperoleh penghasilan tambahan yang stabil dan meraih
-              kesuksesan bisnis Anda sendiri. Tunggu apa lagi? Gabung bersama
-              kami dan mulailah perjalanan Anda menuju keberhasilan!
-            </p>
-            <a
-              href="https://api.whatsapp.com/send/?phone=081211118412&text&type=phone_number&app_absent=0"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="buttonContainer"
-            >
-              <button className="btnWA2 hidden">
-                <BsWhatsapp className="iconWa" />
-                GABUNG SEKARANG
-              </button>
-            </a>
-          </div>
-        </div>
-
-        <div className="konsulWrapper row">
-          <div className="col-xl col-lg col-md-1">
-            <h1 className="headF2 ">KONSULTASI LANGSUNG DENGAN KAMI</h1>
-            <p className="txtF2">
-              Anda dapat melakukan konsultasi langsung dengan kami sebelum
-              pembelian atau sesudah pembelian.
-              <br /> Kami akan membantu menyelesaikan permasalahan anda. Jangan
-              ragu untuk menghubungi kami kapan saja,
-              <br /> karena kami selalu siap untuk membantu Anda dan pembeli
-              Anda.
-            </p>
-            <a
-              href="https://api.whatsapp.com/send/?phone=081211118412&text&type=phone_number&app_absent=0"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="buttonContainer"
-            >
-              <button className="btnWA3 hidden">
-                <BsWhatsapp className="iconWa" />
-                KONSULTASI DISINI
-              </button>
-            </a>
+      <div style={{ backgroundColor: "#FFE2EB", marginTop: "60px", padding: "20px" }}>
+        <div className="resellerF">
+          <h1 className="headF hidden" style={{marginBottom: "60px"}}>BERGABUNGLAH DENGAN KAMI SEKARANG</h1>
+          <div className="joinWrapper">
+            <div className="imgContainer">
+              <img className="resellerCoa hidden" src={coa5} alt="Avatar" />
+            </div>
+            <div className="textContainer">
+              <p className="txtF hidden">
+                Bergabunglah dengan kami sekarang dan dapatkan peluang
+                untuk menawarkan produk berkualitas tinggi kepada pelanggan Anda. Selain itu, kami menyediakan dukungan penuh untuk reseller, mulai dari materi promosi,
+                strategi pemasaran, hingga panduan bisnis yang akan membantu Anda berkembang. Tidak perlu khawatir
+                dengan modal besar, karena sistem kami memungkinkan Anda untuk memulai dengan investasi yang terjangkau.
+              </p>
+              <a
+                href="https://api.whatsapp.com/send/?phone=081211118412&text&type=phone_number&app_absent=0"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="buttonContainer hidden"
+              >
+                <button className="btnWA">
+                  <BsWhatsapp className="iconWa" />
+                  GABUNG SEKARANG
+                </button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
+
     </main>
   );
 }
